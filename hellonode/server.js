281a419426649327f6db96ac7d2963b8b44e8c16
adigitalife <aylwin.cal@gmail.com>
2018-01-31 09:45:30 +0200

    Create server.js

---
+++ hellonode/server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var handle_request = function(request, response){
+console.log('Received request for URL: ' + request.url);
+response.writeHead(200);
+response.end('Hello World!');
+};
+var www = http.createServer(handle_request);
+www.listen(8080);
